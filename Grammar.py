7ad6e0bc4040a3d18fd57624fe2bdc5bfb8b873c
xiaocong-Fu <65238860+xiaocong-Fu@users.noreply.github.com>
2020-07-16 22:57:35 +0800

    Update Grammar.py
    测试代码-----被测试的代码

--- Grammar.py
+++ Grammar.py
@@ -690,3 +690,11 @@
 #     else:
 #         print('we remenber you favorite number is :' + str(contents))
 
+# ------------------------------------------------测试代码---------------------------------------------
+# 测试函数
+def get_formatted_name(first,last,middle = ''):
+    if middle:
+        full_name = first + ' ' + middle + ' '+ last
+    else:
+        full_name = first + ' ' + last
+    return full_name.title()
