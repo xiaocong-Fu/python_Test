4420d2875aa5aaf8199792427c5dda63a89b6da2
fuxiaocong <you@example.com>
2020-07-18 19:51:25 +0800

    Update test.py

--- class_base/test.py
+++ class_base/test.py
@@ -1,15 +1,20 @@
-import unittest
-from Grammar import get_formatted_name
-class NameTestCase(unittest.TestCase):
-    '''测试Grammar.get_formatted_name'''
-    def test_first_last_name(self):
-        '''能够处理janis Joplin的姓名'''
-        formatted_name = get_formatted_name('janis','joplin')
-        self.assertEqual(formatted_name,'Janis Joplin')                             # 断言方法：用来核实得到的结果与期望的结果一致
+# -----------------------------------测试代码-------------------------------------
+# import unittest
+# from Grammar import get_formatted_name
+# class NameTestCase(unittest.TestCase):
+#     '''测试Grammar.get_formatted_name'''
+#     def test_first_last_name(self):
+#         '''能够处理janis Joplin的姓名'''
+#         formatted_name = get_formatted_name('janis','joplin')
+#         self.assertEqual(formatted_name,'Janis Joplin')                             # 断言方法：用来核实得到的结果与期望的结果一致
+#
+#     def test_first_last_middle_name(self):
+#         '''能够正确的处理wolfgang amadeus mozart的姓名'''
+#         formatted_name = get_formatted_name('wolfgang','amadeus','mozart')
+#         self.assertEqual(formatted_name,'Wolfgang Mozart Amadeus')
+#
+# unittest.main()
 
-    def test_first_last_middle_name(self):
-        '''能够正确的处理wolfgang amadeus mozart的姓名'''
-        formatted_name = get_formatted_name('wolfgang','amadeus','mozart')
-        self.assertEqual(formatted_name,'Wolfgang Mozart Amadeus')
 
-unittest.main()
+
+# -------------------------------------各种断言方法-----------------------------------
