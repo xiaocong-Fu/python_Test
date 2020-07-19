f1b699e84c1e8f3070bdca6f4a39cc0bacf82bdf
xiaocong-Fu <65238860+xiaocong-Fu@users.noreply.github.com>
2020-07-19 22:53:24 +0800

    Create survey.py
    survey.py的被测试代码

---
+++ class_base/survey.py
@@ -0,0 +1,16 @@
+class AnonymousSurvey():
+    '''收集匿名调查问卷的答案'''
+    def __init__(self,queestion):
+        self.question = queestion
+        self.responses = []
+
+    def show_question(self):
+        print(self.question)
+
+    def store_response(self,new_response):
+        self.responses.append(new_response)
+
+    def show_results(self):
+        print('Survey results: ')
+        for response in self.responses:
+            print('---' + response)
